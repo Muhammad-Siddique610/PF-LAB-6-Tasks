a2879cf5d2d3f8dc74340ad2416e53bb2c02ff97
Muhammad Siddique <muhammadsiddique2006@gmail.com>
2025-10-04 00:51:38 +0500

    Update task1.c

--- task1.c
+++ task1.c
@@ -4,11 +4,6 @@ int main()
 {
     int i;
     int n = 20;
-
-    //printf("What is the maximum range you want to see even numbers between? ");
-    //scanf("%d",&n)
-
-    //mam i kept the n value as hardcoded if it is needed to be input based just uncomment above.
     printf("Even Numbers from 2 to 20 are");
     for(i = 2 ; i <= n ; i++)
     {
@@ -17,4 +12,5 @@ int main()
             printf(" %d ",i);
         }
     }
+
 }
